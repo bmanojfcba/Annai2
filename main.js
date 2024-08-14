da13b42242d44ca772745efb1a2991beeaa086c7
MANOJKUMAR B <bmanoj.2k@gmail.com>
2024-08-14 18:14:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+//PROJECT SLIDER
+        $('#project-slider').owlCarousel({
+             loop:true,
+            margin:0,
+            nav:false,
+            dots: false,
+            smartSpeed: 1000,
+            responsive:{
+                0:{
+
+                },
+                768: {
+                        I
+                    },
+                1140: {
+                        items: 2,
+                    }
+            }
+         })
+    });
